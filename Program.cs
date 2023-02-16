88e4c7739d580e9677e7eb56ce53d2a3c1a077a6
Kristy Prieto <kprieto14@gmail.com>
2023-02-15 20:04:36 -0500

    Finished adventure mode where I added new code to prompt user to input max guess value then store that as a variable to be used in the formula later. Also added a variable that calculates how many guesses it should take to guess. Lastly, I added a loop that asks if the player would like to play again then call back a method where I stuffed all of my original code into.

--- Program.cs
+++ Program.cs
@@ -8,11 +8,10 @@ namespace NumberGuesser
     //Creates a greeting method and allows the user to type in what they want the max number of the computer to guess and returns that value entered by user to be re-used
     static string Greeting(string prompt)
     {
-      Console.WriteLine("Hello! Welcome to my Guessing Game, where I will try to guess a number you're thinking about!");
       Console.Write(prompt);
 
       var response = Console.ReadLine();
-
+    
       return response;
     }
 
@@ -24,10 +23,17 @@ namespace NumberGuesser
       return null;
     }
 
-    static void Main(string[] args)
+    //The method that houses all the code I worked on before to be repeated if the user wants to re-start the game.
+    static void guessingGame()
     {
       //Starts the program with a greeting to let the user know its purpose and read their response
       var maxNumberResponse = Greeting("What is the max number you would like for me to guess? ");
+
+      //Turns response from user about max number range into a double to be used in a list later for math, and then calculates how many tries should occur according to the Binary Search theory.
+      double parsedResponse = double.Parse(maxNumberResponse);
+      double estimatedNumberOfTries = Math.Ceiling(Math.Log2(parsedResponse));
+
+      Console.WriteLine($"That should only take me about {estimatedNumberOfTries} guesses!"); 
       Console.Write($"Please think of a WHOLE number between 1 - {maxNumberResponse}. Enter GO when you are ready: ");
       
       //Creates a boolean statement that will be used for the while statement below    
@@ -49,8 +55,7 @@ namespace NumberGuesser
         }
       }
 
-      //Turns response from user about max number range into a double to be used in a list later for math
-      double parsedResponse = double.Parse(maxNumberResponse);
+
       //Creates two new lists to keep track of. The first list will keep track of the minimum and maximum value. The 2nd list keeps track of the guessed numbers to count later.
       var minMaxValueNumbers = new List<double>() {1, parsedResponse};
       var guessedNumbers = new List<double>();
@@ -59,8 +64,8 @@ namespace NumberGuesser
       bool nextUserCommand = false;
       while (nextUserCommand == false)
       {
-        //Formula that calculates a new number to guess
-        double newGuessNumber = Math.Round((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2);
+        //Formula that calculates a new number to guess, based off the Binary search math equation
+        double newGuessNumber = Math.Ceiling((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2);
 
         Console.WriteLine($"Is the number you're thinking {newGuessNumber}?");
         Console.Write($"Please enter HIGHER, if the number you're thinking of is higher then {newGuessNumber}, LOWER if it is lower than {newGuessNumber}, or CORRECT if I am right: ");
@@ -70,29 +75,66 @@ namespace NumberGuesser
 
         if (otherUpperCaseUserInput == "HIGHER")
         {
+          //Adds the guess number to list to be counted later, formula underneath is to replace the minimum value if the number is higher than what is guessed
           guessedNumbers.Add(newGuessNumber);
-          minMaxValueNumbers[0] = Math.Round(((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2) + 1);
+          minMaxValueNumbers[0] = Math.Ceiling(((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2) + 1);
         }
         else if (otherUpperCaseUserInput == "LOWER")
         {
+          //Adds the guess number to list to be counted later, formula underneath is to replace the maximum value if the number is lower than what is guessed
           guessedNumbers.Add(newGuessNumber);
-          minMaxValueNumbers[1] = Math.Round(((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2) - 1);
+          minMaxValueNumbers[1] = Math.Ceiling(((minMaxValueNumbers[0] + minMaxValueNumbers[1]) / 2) - 1);
         }
         else if (otherUpperCaseUserInput == "CORRECT")
         {
           guessedNumbers.Add(newGuessNumber);
           var amountOfNumbersGuessed = guessedNumbers.Count; 
 
-          Console.WriteLine($"Hurrah! I am victorious and glorious! That only took me {amountOfNumbersGuessed} tries!");
-          nextUserCommand = true;
+          //Nested if statement for fun, compares if program accurately guessed how long it would take to guess and curates a response accordingly.
+          if (estimatedNumberOfTries == amountOfNumbersGuessed)
+          {
+            Console.WriteLine($"Hurrah! I am victorious and glorious! Looks like I was right! That only took me {amountOfNumbersGuessed} tries!");
+            nextUserCommand = true;
+          }
+          else
+          {
+            Console.WriteLine($"Uh oh, looks like I was off by a little bit! That actually took me {amountOfNumbersGuessed} tries!");
+            nextUserCommand = true;
+          }
         }
         else
         {
           var answer = UnknownCommand("I am not sure what you are saying. Please enter if some number is HIGHER, LOWER, or CORRECT: ");
         }
       }
-      
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello! Welcome to my Guessing Game, where I will try to guess a number you're thinking about!");
+      guessingGame();
 
+      bool playAgain = true;
+      while (playAgain == true)
+      {
+        //Once game finishes, this is block of code that asks user if they would like to replay and adjusts accordingly.
+        Console.WriteLine("Would you like to play again? Enter YES to restart or NO to quit.");
+        var repeatAgainResponse = Console.ReadLine();
+        var repeatResponseUpper = repeatAgainResponse.ToUpper();
+
+        if (repeatResponseUpper == "YES")
+        {
+          guessingGame();
+        }
+        else if (repeatResponseUpper == "NO")
+        {
+          Console.Write("It was fun to play with you! Come back anytime!");
+          playAgain = false;
+        }
+        else 
+        {
+          var answer = UnknownCommand("I'm sorry, I do not understand.");
+        }
+      }
     }
   }
 }
