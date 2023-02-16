67e789096bcb7eb684c2df465584804f19a92617
Kristy Prieto <kprieto14@gmail.com>
2023-02-15 19:07:14 -0500

    Changed code to accept an input from user to choose the max number of guesses

--- Program.cs
+++ Program.cs
@@ -5,20 +5,34 @@ namespace NumberGuesser
 {
   class Program
   {
-    //Creates a greeting method and allows the user to type in when they are ready.
-    static void Greeting()
+    //Creates a greeting method and allows the user to type in what they want the max number of the computer to guess and returns that value entered by user to be re-used
+    static string Greeting(string prompt)
     {
       Console.WriteLine("Hello! Welcome to my Guessing Game, where I will try to guess a number you're thinking about!");
-      /*Console.WriteLine("What is the max number you would like for me to guess? ");
-      var response = Console.ReadLine();
-      double parsedResponse = double.Parse(response);*/
-      
-      Console.Write($"Please think of a WHOLE number between 1 - 1024. Enter GO when you are ready: ");
+      Console.Write(prompt);
 
+      var response = Console.ReadLine();
+
+      return response;
+    }
+
+    //Creates a method that calls back when the user attempts to enter text not recognized by program and returns nothing.
+    static string UnknownCommand(string prompt)
+    {
+      Console.WriteLine(prompt);
+
+      return null;
+    }
+
+    static void Main(string[] args)
+    {
+      //Starts the program with a greeting to let the user know its purpose and read their response
+      var maxNumberResponse = Greeting("What is the max number you would like for me to guess? ");
+      Console.Write($"Please think of a WHOLE number between 1 - {maxNumberResponse}. Enter GO when you are ready: ");
+      
       //Creates a boolean statement that will be used for the while statement below    
       bool userCommand = false; 
-
-      //While statement that reads user's input to start the guessing process when they are ready. Honestly might change this later to include please press ENTER to start so I can reuse this statement.
+      //While statement that reads user's input to start the guessing process when they are ready.
       while (userCommand == false)
       {
         var userInput = Console.ReadLine();
@@ -34,22 +48,11 @@ namespace NumberGuesser
           var answer = UnknownCommand("I'm sorry, I do not understand. Please enter GO when you are ready.");
         }
       }
-    }
-    //Creates a method that calls back when the user attempts to enter text not recognized by program and returns nothing.
-    static string UnknownCommand(string prompt)
-    {
-      Console.WriteLine(prompt);
 
-      return null;
-    }
-
-    static void Main(string[] args)
-    {
-      //Starts the program with a greeting to let the user know its purpose and read their response
-      Greeting();
-      
+      //Turns response from user about max number range into a double to be used in a list later for math
+      double parsedResponse = double.Parse(maxNumberResponse);
       //Creates two new lists to keep track of. The first list will keep track of the minimum and maximum value. The 2nd list keeps track of the guessed numbers to count later.
-      var minMaxValueNumbers = new List<double>() {1, 1024};
+      var minMaxValueNumbers = new List<double>() {1, parsedResponse};
       var guessedNumbers = new List<double>();
 
       //Start of formula to guess person's number
